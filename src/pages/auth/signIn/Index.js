32912b1e98f5b12c27dedf3509faa0d25914a2c6
umashankartechnoelevate <umashankarjabagond@gmail.com>
2023-04-04 17:26:36 +0530

    project structure

---
+++ src/pages/auth/signIn/Index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name="firstName" value="" />
+                <input type="text" name="lastName" value="" />
+                <input type="text" name="email" value="" />
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
---
+++ src/routes/routeObject/index.js
@@ -0,0 +1,15 @@
+import SignIn from "../../pages/auth/signIn/Index"
+
+const AuthRoutes = [
+    {
+        path: "/",
+        element: <SignIn />
+    }
+]
+
+const routeObject = [
+    ...AuthRoutes
+
+]
+
+export default { routeObject }
